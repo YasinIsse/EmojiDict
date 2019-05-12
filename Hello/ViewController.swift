dd35ccd4d91dec8fba49187f7c89c9b7c37132fd
Yasin Isse (yai472) <yai472@usask.ca>
2019-05-12 04:30:55 -0600

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Yasin Isse on 2019-05-12.
+//  Copyright © 2019 Yasin Isse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
